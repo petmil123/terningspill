70e70a438d12927682e8e1b65cfe645b63eb1185
Petar Milic <petarm@stud.ntnu.no>
2025-11-09 10:59:02 +0100

    Updated to for range in gameInitiatior

--- server/gameInitiator/gameInitiator.go
+++ server/gameInitiator/gameInitiator.go
@@ -15,32 +15,30 @@ type ConnectionRequest struct {
 
 func GameManager(connectRequestChan chan ConnectionRequest) {
 	players := make([]*gameManager.Player, 0, 2)
-	for {
-		select {
-		case req := <-connectRequestChan:
-			if len(players) >= 2 {
-				// Reject connection
-				fmt.Println("Rejected")
-				req.Player.SendChan <- socketHandler.PlayerMessage{
-					ConnectionID: req.Player.ConnectionID,
-					Message:      socketHandler.Message{Action: "error", Data: "Maximum players reached"},
-				}
-				req.AcceptCh <- false
-			} else {
-				players = append(players, &req.Player)
-				// Acknowledge connection
-				fmt.Println("Accepted connection for player:", req.Player.ConnectionID)
-				req.Player.SendChan <- socketHandler.PlayerMessage{
-					ConnectionID: req.Player.ConnectionID,
-					Message:      socketHandler.Message{Action: "connected", Data: "Welcome Player"},
-				}
-				req.AcceptCh <- true
-				if len(players) == 2 {
-					fmt.Println("Two players connected. Starting game...")
-					go gameManager.RunGame(players[0], players[1])
-					// Reset players for next game
-					players = make([]*gameManager.Player, 0, 2)
-				}
+	for range connectRequestChan {
+		req := <-connectRequestChan
+		if len(players) >= 2 {
+			// Reject connection
+			fmt.Println("Rejected")
+			req.Player.SendChan <- socketHandler.PlayerMessage{
+				ConnectionID: req.Player.ConnectionID,
+				Message:      socketHandler.Message{Action: "error", Data: "Maximum players reached"},
+			}
+			req.AcceptCh <- false
+		} else {
+			players = append(players, &req.Player)
+			// Acknowledge connection
+			fmt.Println("Accepted connection for player:", req.Player.ConnectionID)
+			req.Player.SendChan <- socketHandler.PlayerMessage{
+				ConnectionID: req.Player.ConnectionID,
+				Message:      socketHandler.Message{Action: "connected", Data: "Welcome Player"},
+			}
+			req.AcceptCh <- true
+			if len(players) == 2 {
+				fmt.Println("Two players connected. Starting game...")
+				go gameManager.RunGame(players[0], players[1])
+				// Reset players for next game
+				players = make([]*gameManager.Player, 0, 2)
 			}
 		}
 	}

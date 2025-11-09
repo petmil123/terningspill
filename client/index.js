d35cd24b52bdbe5e1cbe47fbe57787b6cf7bd6ed
Petar Milic <petarm@stud.ntnu.no>
2025-11-09 15:37:43 +0100

    Add base game logic

--- client/index.js
+++ client/index.js
@@ -24,24 +24,36 @@ ws.onmessage = (event) => {
     // Update the game UI based on the new game state
     updateBoard(message.data);
   }
+  else {
+    console.warn("Unexpected message or state:", state, message);
+  }
 };
 ws.onclose = () => {
-    console.log("WebSocket connection closed");
+  console.log("WebSocket connection closed");
 };
 
 ws.onerror = (error) => {
   console.error("WebSocket error:", error);
 };
 
+document.querySelectorAll("button").forEach(btn => {
+  btn.onclick = () => {
+    console.log(`Button ${btn.id} clicked`);
+    ws.send(JSON.stringify({ action: "guess", data: btn.id }));
+  };
+});
+
 function updateBoard(gameState) {
   // Extract the different variables based on the gameState string
-  const [playerCoveredRaw, opponentCoveredRaw, isYourTurnRaw, turnPhase, chosenField, diceRoll] = gameState.split(",");
+  const [playerCoveredRaw, opponentCoveredRaw, isYourTurnRaw, turnPhase, chosenFieldRaw, diceRollRaw] = gameState.split(",");
   const playerCovered = playerCoveredRaw.slice(1, -1).split(" ").map(Number);
   const opponentCovered = opponentCoveredRaw.slice(1, -1).split(" ").map(Number);
+  const chosenField = Number(chosenFieldRaw);
+  const diceRoll = Number(diceRollRaw);
   const isYourTurn = isYourTurnRaw === "true";
-  console.log("Updating board with:", {playerCovered, opponentCovered, isYourTurn, turnPhase, chosenField, diceRoll });
+  console.log("Updating board with:", { playerCovered, opponentCovered, isYourTurn, turnPhase, chosenField, diceRoll });
   // Cover the fields based on covered
-
+  coverBoard(playerCovered, opponentCovered);
   // How do we do the covered?
   // If its not your turn, disable all controls.
   if (!isYourTurn) {
@@ -56,7 +68,6 @@ function updateBoard(gameState) {
         disableButtons();
         break;
       case "toCover":
-        disableDie();
         enableBoard();
         disableButtons();
         setDie(diceRoll);
@@ -67,17 +78,19 @@ function updateBoard(gameState) {
         enableButtons();
         setCall(chosenField);
         break;
+    }
   }
-}
 }
 
 function disableDie() {
   const die = document.getElementById("ownDice");
   die.removeEventListener("click", throwDie);
-  setDie(die, 0);
+  setDie(0);
 }
-function setDie(die, value) {
-  if (value < 1 || value > 6 ) {
+function setDie(value) {
+  const die = document.getElementById("ownDice");
+  console.log("Setting die to value:", value);
+  if (value < 1 || value > 6) {
     die.src = "dices/none.png";
     die.alt = "Dice showing none";
   } else {
@@ -90,15 +103,61 @@ function enableDie() {
   const die = document.getElementById("ownDice");
   die.addEventListener("click", throwDie);
   die.classList.add("clickable")
-  setDie(die, Math.ceil(Math.random() * 6));
+  setDie(Math.ceil(Math.random() * 6));
 }
+
+function coverBoard(ownCoveredFields, oppCoveredFields) {
+  for (let i = 0; i < 6 ; i++) {
+    if(ownCoveredFields[i] === 0) {
+      document.getElementById(`ownField_1_${i+1}`).classList.remove("covered");
+      document.getElementById(`ownField_2_${i+1}`).classList.remove("covered");
+    }
+    if(ownCoveredFields[i] === 1) {
+      document.getElementById(`ownField_1_${i+1}`).classList.add("covered");
+      document.getElementById(`ownField_2_${i+1}`).classList.remove("covered");
+
+    }
+    if(ownCoveredFields[i] === 2) {
+      document.getElementById(`ownField_1_${i+1}`).classList.add("covered");
+      document.getElementById(`ownField_2_${i+1}`).classList.add("covered");
+
+    }
+  }
+  for (let i = 0; i < 6 ; i++) {
+    if(oppCoveredFields[i] >= 1) {
+      document.getElementById(`oppField_1_${i+1}`).classList.add("covered");
+    }
+    if(oppCoveredFields[i] >= 2) {
+      document.getElementById(`oppField_2_${i+1}`).classList.add("covered");
+    }
+  }
+}
+
 
 function disableBoard() {
   document.querySelectorAll(".ownRow .field").forEach(field => field.classList.add("disabled"));
+  for (let i = 1; i <= 6; i++) {
+    const field1 = document.getElementById(`ownField_1_${i}`);
+    const field2 = document.getElementById(`ownField_2_${i}`);
+    field1.onclick = null;
+    field2.onclick = null;
+  }
 }
 
 function enableBoard() {
   document.querySelectorAll(".ownRow .field").forEach(field => field.classList.remove("disabled"));
+  for (let i = 1; i <= 6; i++) {
+    const field1 = document.getElementById(`ownField_1_${i}`);
+    const field2 = document.getElementById(`ownField_2_${i}`);
+    field1.onclick = () => {
+      console.log(`Field ${i} clicked`);
+      ws.send(JSON.stringify({ action: "coverField", data: String(i) }));
+    };
+    field2.onclick = () => {
+      console.log(`Field ${i} clicked`);
+      ws.send(JSON.stringify({ action: "coverField", data: String(i) }));
+    };
+  }
 }
 
 function enableButtons() {
--- server/gameManager/gameManager.go
+++ server/gameManager/gameManager.go
@@ -4,6 +4,7 @@ import (
 	"dice/socketHandler"
 	"fmt"
 	"math/rand"
+	"strconv"
 )
 
 type Player struct {
@@ -64,14 +65,169 @@ func RunGame(player1 *Player, player2 *Player) {
 	}
 
 	gameState := initializeGameState()
-	fmt.Println(gameState.toString(1))
-	fmt.Println(gameState.toString(2))
-	player1.SendChan <- socketHandler.PlayerMessage{
-		ConnectionID: player1.ConnectionID,
-		Message:      socketHandler.Message{Action: "gameState", Data: gameState.toString(1)},
-	}
-	player2.SendChan <- socketHandler.PlayerMessage{
-		ConnectionID: player2.ConnectionID,
-		Message:      socketHandler.Message{Action: "gameState", Data: gameState.toString(2)},
+
+	for {
+		player1.SendChan <- socketHandler.PlayerMessage{
+			ConnectionID: player1.ConnectionID,
+			Message:      socketHandler.Message{Action: "gameState", Data: gameState.toString(1)},
+		}
+		player2.SendChan <- socketHandler.PlayerMessage{
+			ConnectionID: player2.ConnectionID,
+			Message:      socketHandler.Message{Action: "gameState", Data: gameState.toString(2)},
+		}
+		select {
+		case msg := <-player1.ReceiveChan:
+			fmt.Println("Received message from Player 1:", msg)
+			if gameState.turn != 1 {
+				player1.SendChan <- socketHandler.PlayerMessage{
+					ConnectionID: player1.ConnectionID,
+					Message:      socketHandler.Message{Action: "error", Data: "Not your turn."},
+				}
+			} else {
+				switch gameState.turnPhase {
+				case toThrow:
+					if msg.Message.Action != "throwDie" {
+						player1.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player1.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid action. You need to throw the die."},
+						}
+					}
+					// Simulate die throw
+					fmt.Println("Player 1 is throwing the die.")
+					gameState.diceRoll = rand.Intn(6) + 1
+					gameState.turnPhase = toCover
+				case toCover:
+					if msg.Message.Action != "coverField" {
+						player1.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player1.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid action. You need to cover a field."},
+						}
+					}
+					// Process covering field
+					fieldToCover, err := strconv.Atoi(msg.Message.Data)
+					if err != nil || fieldToCover < 1 || fieldToCover > 6 {
+						player1.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player1.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid field number."},
+						}
+					} else {
+						gameState.chosenField = fieldToCover
+						if gameState.player1Covered[fieldToCover-1] < 2 {
+							gameState.player1Covered[fieldToCover-1]++
+						}
+						gameState.turnPhase = toCall
+						gameState.turn = 2 // Switch turn to player 2
+					}
+				case toCall:
+					if msg.Message.Action != "guess" {
+						player1.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player1.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid action. You need to make a guess."},
+						}
+					}
+					if !(msg.Message.Data == "bluff" || msg.Message.Data == "truth") {
+						player1.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player1.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid guess. Must be 'bluff' or 'truth'."},
+						}
+					}
+					// Process guess
+					if msg.Message.Data == "truth" {
+						gameState.turnPhase = toThrow
+					}
+					isBluff := gameState.diceRoll != gameState.chosenField
+					if isBluff {
+						//Guess was correct, remove the chosen cover.
+						gameState.player2Covered[gameState.chosenField-1]--
+					} else {
+						for {
+							//Guess was wrong, remove a random cover from player 1.
+							field := rand.Intn(6)
+							if gameState.player1Covered[field] > 0 {
+								gameState.player1Covered[field]--
+								break
+							}
+						}
+					}
+					gameState.turnPhase = toThrow
+				}
+			}
+		case msg := <-player2.ReceiveChan:
+			fmt.Println("Received message from Player 2:", msg)
+			if gameState.turn != 2 {
+				player2.SendChan <- socketHandler.PlayerMessage{
+					ConnectionID: player2.ConnectionID,
+					Message:      socketHandler.Message{Action: "error", Data: "Not your turn."},
+				}
+			} else {
+				switch gameState.turnPhase {
+				case toThrow:
+					if msg.Message.Action != "throwDie" {
+						player2.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player2.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid action. You need to throw the die."},
+						}
+					}
+					// Simulate die throw
+					fmt.Println("Player 2 is throwing the die.")
+					gameState.diceRoll = rand.Intn(6) + 1
+					gameState.turnPhase = toCover
+				case toCover:
+					if msg.Message.Action != "coverField" {
+						player2.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player2.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid action. You need to cover a field."},
+						}
+					}
+					// Process covering field
+					fieldToCover, err := strconv.Atoi(msg.Message.Data)
+					if err != nil || fieldToCover < 1 || fieldToCover > 6 {
+						player2.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player2.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid field number."},
+						}
+					} else {
+						gameState.chosenField = fieldToCover
+						gameState.turnPhase = toCall
+						if gameState.player2Covered[fieldToCover-1] < 2 {
+							gameState.player2Covered[fieldToCover-1]++
+						}
+						gameState.turn = 1 // Switch turn to player 1
+					}
+				case toCall:
+					if msg.Message.Action != "guess" {
+						player2.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player2.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid action. You need to make a guess."},
+						}
+					}
+					if !(msg.Message.Data == "bluff" || msg.Message.Data == "truth") {
+						player2.SendChan <- socketHandler.PlayerMessage{
+							ConnectionID: player2.ConnectionID,
+							Message:      socketHandler.Message{Action: "error", Data: "Invalid guess. Must be 'bluff' or 'truth'."},
+						}
+					}
+					// Process guess
+					if msg.Message.Data == "truth" {
+						gameState.turnPhase = toThrow
+					}
+					isBluff := gameState.diceRoll != gameState.chosenField
+					if isBluff {
+						//Guess was correct, remove the chosen cover.
+						gameState.player1Covered[gameState.chosenField-1]--
+					} else {
+						for {
+							//Guess was wrong, remove a random cover from player 2.
+							field := rand.Intn(6)
+							if gameState.player2Covered[field] > 0 {
+								gameState.player2Covered[field]--
+								break
+							}
+						}
+					}
+					gameState.turnPhase = toThrow
+				}
+			}
+		}
 	}
 }
